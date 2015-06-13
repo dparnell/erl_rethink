2c488273bddf7e80f6541c6df7fc6009c572edbe
Daniel Parnell <me@danielparnell.com>
2015-06-13 15:38:13 +1000

    Added more term types to the compiler

--- src/erl_rethink_compiler.erl
+++ src/erl_rethink_compiler.erl
@@ -5,28 +5,102 @@
         ]).
 
 -define(DATUM, 1).
+-define(MAKE_ARRAY, 2).
+-define(VAR, 10).
 -define(DB, 14).
 -define(TABLE, 15).
+-define(GET, 16).
+-define(EQ, 17).
+-define(NE, 18).
+-define(LT, 19).
+-define(LE, 20).
+-define(GT, 21).
+-define(GE, 22).
+-define(NOT, 23).
+-define(FILTER, 39).
+-define(DB_CREATE, 57).
 -define(DB_LIST, 59).
+-define(TABLE_CREATE, 60).
 -define(TABLE_LIST, 62).
+-define(FUNC, 69).
 -define(LIMIT, 71).
 
-compile({datum, Datum}) ->
-    [?DATUM, compile(Datum)];
-compile({db, Database}) ->
-    [?DB, [Database]];
-compile({table_list, Database}) ->
-    [?TABLE_LIST, [compile(Database)]];
-compile(table_list) ->
-    [?TABLE_LIST];
-compile(db_list) ->
-    [?DB_LIST];
-compile({limit, Sequence, Count}) ->
-    [?LIMIT, [compile(Sequence), Count]];
-compile({table, Table}) ->
-    [?TABLE, [Table]];
-compile({table, Database, Table}) ->
-    [?TABLE, [compile(Database), Table]];
+-record(state, {
+          variable_counter,
+          variables
+         }).
 
-compile(_) ->
-    erlang:error(unhandled_term).
+compile(Query) ->
+    compile(Query, #state{
+              variable_counter = 0,
+              variables = []
+             }).
+
+add_new_variable(State, Name) ->
+    VarNum = State#state.variable_counter + 1,
+    Variables = lists:append([{Name, VarNum}], State#state.variables),
+
+    {ok, State#state{variable_counter = VarNum, variables = Variables}, VarNum}.
+
+add_new_variables(State, Variables) ->
+    lists:foldl(fun(X, {State3, Vars}) ->
+                        {ok, State4, VarNum} = add_new_variable(State3, X),
+                        {State4, [VarNum | Vars]}
+                end, {State, []}, Variables).
+
+find_variable(State, Name) ->
+    VarNum = proplists:get_value(Name, State#state.variables),
+    case VarNum of
+        undefined -> erlang:error({unknown_variable, Name});
+        _ -> VarNum
+    end.
+
+compile(Value, _State) when is_binary(Value) ->
+    Value;
+compile(Value, _State) when is_number(Value) ->
+    Value;
+compile({datum, Datum}, State) ->
+    [?DATUM, compile(Datum, State)];
+compile({db, Database}, _State) ->
+    [?DB, [Database]];
+compile({table_create, Name}, _State) ->
+    [?TABLE_CREATE, [Name]];
+compile({table_create, Database, Name}, State) ->
+    [?TABLE_CREATE, [compile(Database, State), Name]];
+compile({table_list, Database}, State) ->
+    [?TABLE_LIST, [compile(Database, State)]];
+compile(table_list, _State) ->
+    [?TABLE_LIST];
+compile({db_create, Name}, _State) ->
+    [?DB_CREATE, [Name]];
+compile(db_list, _State) ->
+    [?DB_LIST];
+compile({limit, Sequence, Count}, State) ->
+    [?LIMIT, [compile(Sequence, State), Count]];
+compile({table, Table}, _State) ->
+    [?TABLE, [Table]];
+compile({table, Database, Table}, State) ->
+    [?TABLE, [compile(Database, State), Table]];
+compile({filter, Sequence, Condition}, State) ->
+    [?FILTER, [compile(Sequence, State), compile(Condition, State)]];
+compile({func, Params, Body}, State) ->
+    {State2, VarNums} = add_new_variables(State, Params),
+    [?FUNC, [[?MAKE_ARRAY, VarNums], compile(Body, State2)]];
+compile({var, Variable}, State) ->
+    [?VAR, [find_variable(State, Variable)]];
+compile({eq, A, B}, State) ->
+    [?EQ, [compile(A, State), compile(B, State)]];
+compile({ne, A, B}, State) ->
+    [?NE, [compile(A, State), compile(B, State)]];
+compile({lt, A, B}, State) ->
+    [?LT, [compile(A, State), compile(B, State)]];
+compile({le, A, B}, State) ->
+    [?LE, [compile(A, State), compile(B, State)]];
+compile({gt, A, B}, State) ->
+    [?GT, [compile(A, State), compile(B, State)]];
+compile({ge, A, B}, State) ->
+    [?GE, [compile(A, State), compile(B, State)]];
+compile({invert, Value}, State) ->
+    [?NOT, [compile(Value, State)]];
+compile(Query, _State) ->
+    erlang:error({unhandled_term, Query}).
